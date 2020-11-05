6cca2d1c9e54bcf588dac137f4fe758ddfdf9642
Vic.Orlands <chimezieinnocent39@gmail.com>
2020-11-05 17:01:43 +0100

    Refactored the code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,45 +8,51 @@ class Modal extends Component {
 		this.props.onClose && this.props.onClose(e);
 	};
 
+	windowClickClose = (e) => {
+		this.props.onClose && this.props.onClose(e);
+	};
+
 	render() {
 		if (this.props.open) {
 			return null;
 		}
 
 		return (
-			<section className="modal-container">
-				<header>
-					<hgroup>
-						<h1>Notify Me</h1>
-						<h3>Please input your details here</h3>
-					</hgroup>
+			<section className="modal-container" id="modal" onClick={this.windowClickClose}>
+				<div class="modal-content">
+					<header>
+						<hgroup>
+							<h1>Notify Me</h1>
+							<h3>Please input your details here</h3>
+						</hgroup>
 
-					<AiOutlineClose id="font" onClick={this.onClose} />
-				</header>
-				<hr />
+						<AiOutlineClose id="font" onClick={this.onClose} />
+					</header>
+					<hr />
 
-				<form action="">
-					<label htmlFor="first_name">
-						First Name
-						<input type="text" />
-					</label>
+					<form action="">
+						<label htmlFor="first_name">
+							First Name
+							<input type="text" />
+						</label>
 
-					<label htmlFor="email">
-						Email Address
-						<input type="email" />
-					</label>
+						<label htmlFor="email">
+							Email Address
+							<input type="email" />
+						</label>
 
-					<label htmlFor="device_type">
-						Device Type
-						<select name="cars">
-							<option value="android">Android</option>
-							<option value="ios">IOS</option>
-							<option value="both">Both</option>
-						</select>
-					</label>
+						<label htmlFor="device_type">
+							Device Type
+							<select name="cars">
+								<option value="android">Android</option>
+								<option value="ios">IOS</option>
+								<option value="both">Both</option>
+							</select>
+						</label>
 
-					<button type="button">Notify me</button>
-				</form>
+						<button type="button">Notify me</button>
+					</form>
+				</div>
 			</section>
 		);
 	}
